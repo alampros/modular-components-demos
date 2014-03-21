4ad032b7e07d181b6bf26dfb244ddd2c435ae53c
Aaron Lampros <alampros@gmail.com>
2014-03-21 11:43:10 -0400

    Add optimized build via r.js

---
+++ app.build.js
@@ -0,0 +1,13 @@
+({
+  appDir: "bin/",
+  baseUrl: "js",
+  dir: "bin/optimized",
+  mainConfigFile: 'bin/js/main.js',
+  removeCombined: true,
+  optimize: 'none',
+  modules: [
+    {
+      name: "main"
+    }
+  ]
+})
