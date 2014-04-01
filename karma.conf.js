5541f3a3b98607d13f1331db4f5deac5c5059a51
Aaron Lampros <alampros@gmail.com>
2014-04-01 15:31:18 -0400

    Compatibility for IE with shims

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       {pattern: 'service/**/*.js', included: false},
       {pattern: 'service/**/*.json', included: false},
       {pattern: 'ui/**/*.js', included: false},
+      {pattern: 'util/**/*.js', included: false},
       {pattern: 'vendor/**/*.js', included: false},
       'tests.js',
       'test-main.js'
